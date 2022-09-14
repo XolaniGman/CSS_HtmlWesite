8b62432e06682d6461aec5bfcb53f0bbf18d1c74
Xolanigman4k <mchunuxolani@outlook.com>
2022-09-14 16:08:39 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  var hamburger = document.querySelector(".hamburger");
+  hamburger.addEventListener("click", function(){
+      document.querySelector("body").classList.toggle("active");
+  })
+  
+// open close navbar
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
